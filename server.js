f3187ea734ede221dc063adf1e4c0ad2af12a920
jarick <josephtarick1@gmail.com>
2023-03-27 14:40:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
+// Import necessary modules
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const path = require('path'); // add this line to import the path module
+const path = require('path');
 
+// Import configuration and routes
 const config = require('./config/config');
-const routes = require('./routes/index');
+const router = require('./routes/index'); // import router instance
 
+// Create an instance of the Express application
 const app = express();
 
+// Set the app configuration
 app.set('config', config);
 
+// Use middleware to log HTTP requests
 app.use(morgan('tiny'));
+
+// Use middleware to allow cross-origin resource sharing
 app.use(cors());
+
+// Use middleware to set security-related HTTP headers
 app.use(helmet({
   contentSecurityPolicy: false,
   frameguard: {
@@ -25,17 +34,21 @@ app.use(helmet({
   noSniff: true,
   xssFilter: true
 }));
+
+// Use middleware to parse request bodies as JSON
 app.use(express.json());
 
-app.use('/api', routes);
+// Use the router instance for the `/api` endpoint
+app.use('/api', router);
 
-// Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('frontend/build'));
 
-// Catch-all route handler to serve the React app's index.html file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
-});
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 app.listen(config.port, () => {
   console.log(`Server started on port ${config.port}`);
